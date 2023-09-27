42a9a2e446e085e52dc3f172760f6f022a06161a
tyukei <0024481294bxyk6037@gmail.com>
2023-09-27 10:47:20 +0900

    update

--- backend.py
+++ backend.py
@@ -1,11 +1,14 @@
 import requests
 import json
 
+
 def send_request(data):
+    url = 'https://us-central1-sic-poc1-verify.cloudfunctions.net/registAction'
+
     headers = {
         'Accept': 'application/json',
     }
-    response = requests.post('https://us-central1-sic-poc1-verify.cloudfunctions.net/registAction', headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data)
     
     try:
         return response.json(), None  # 成功した場合はJSONデータとエラーなし(None)を返す
